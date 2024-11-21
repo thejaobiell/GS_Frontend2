44e59000b93ff16eba2db748408a062255cd6119
LucasLDC <LucasL.Chagas@outlook.com>
2024-11-21 12:43:52 -0300

    Update Header.tsx

--- greenpower/src/app/Header/Header.tsx
+++ greenpower/src/app/Header/Header.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import styles from './Header.module.css';
@@ -12,30 +13,38 @@ const fonto = localFont({
 
 const Header = () => {
     const router = useRouter();
+    const [estaLogado, setEstaLogado] = useState(false);
+
+    useEffect(() => {
+        const logado = sessionStorage.getItem('logado');
+        setEstaLogado(logado === 'sim');
+    }, []);
 
     const voltarMenu = () => {
         router.push('/');
-    }
+    };
 
     return (
         <header className={styles.header}>
-            <img className={styles.logo} src="/Logos/logoHeader.png" alt="Logo GreenPower" onClick={voltarMenu} />
-            <h1 className={`${styles.GreenPower} ${fonto.variable}`} onClick={voltarMenu}>
-                GreenPower
-            </h1>
-            <nav>
-                <ul className={styles.navUl}>
-                    <li className={styles.navLi}>
-                        <Link className={styles.navLink} href="/">Menu</Link>
-                    </li>
-                    <li className={styles.navLi}>
-                        <Link className={styles.navLink} href="/Login">Login</Link>
-                    </li>
-                    <li className={styles.navLi}>
-                        <Link className={styles.navLink} href="/Membros">Membros</Link>
-                    </li>
-                </ul>
-            </nav>
+            <div className={styles.logoEtitulo} onClick={voltarMenu}>
+                <img className={styles.logo} src="/Logos/logoHeader.png" alt="Logo GreenPower" />
+                <h1 className={`${styles.GreenPower} ${fonto.variable}`}>GreenPower</h1>
+            </div>
+            {!estaLogado && (
+                <nav>
+                    <ul className={styles.navUl}>
+                        <li className={styles.navLi}>
+                            <Link className={styles.navLink} href="/">Menu</Link>
+                        </li>
+                        <li className={styles.navLi}>
+                            <Link className={styles.navLink} href="/Membros">Membros</Link>
+                        </li>
+                        <li className={styles.navLi}>
+                            <Link className={styles.navLink} href="/Login">Login</Link>
+                        </li>
+                    </ul>
+                </nav>
+            )}
         </header>
     );
 };
